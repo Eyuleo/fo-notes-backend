ca021fd557ba61f9170429d16ad4ca365d85e647
Eyuleo <eyuleo10@gmail.com>
2024-04-07 12:14:27 +0300

    add mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note.js')
 const app = express()
 
 app.use(express.json())
@@ -9,26 +10,10 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only JavaScript',
-		important: false,
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		important: true,
-	},
-]
-
 app.get('/api/notes', (req, res) => {
-	res.json(notes)
+	Note.find({}).then((notes) => {
+		res.json(notes)
+	})
 })
 
 app.get('/api/notes/:id', (req, res) => {
