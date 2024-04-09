697ce84d3a735111bdfc4db728e7e267229d0f8f
Eyuleo <eyuleo10@gmail.com>
2024-04-09 19:27:27 +0300

    add validation

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/notes/:id', (req, res, next) => {
 		.catch((error) => next(error))
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
 	const body = req.body
 
 	if (!body.content) {
@@ -39,22 +39,22 @@ app.post('/api/notes', (req, res) => {
 		content: body.content,
 		important: body.important || false,
 	})
-	note.save().then((savedNote) => {
-		res.json(savedNote)
-	})
+	note
+		.save()
+		.then((savedNote) => {
+			res.json(savedNote)
+		})
+		.catch((error) => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-	const body = req.body
+	const { content, important } = req.body
 
-	const note = {
-		content: body.content,
-		important: body.important,
-	}
-
-	console.log(note)
-
-	Note.findByIdAndUpdate(req.params.id, note, { new: true })
+	Note.findByIdAndUpdate(
+		req.params.id,
+		{ content, important },
+		{ new: true, runValidators: true, context: 'query' }
+	)
 		.then((updatedNote) => {
 			res.json(updatedNote)
 		})
@@ -73,6 +73,8 @@ const errorHandler = (error, req, res, next) => {
 	console.error(error.message)
 	if (error.name === 'Cast error') {
 		return res.status(400).send({ error: 'malformatted id' })
+	} else if (error === 'ValidationError') {
+		return res.status(400).json({ error: error.message })
 	}
 	next(error)
 }
