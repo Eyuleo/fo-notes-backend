3fe5dff8de827ecc8f2ebbd0be04b5c1fd1cd7bc
Eyuleo <eyuleo10@gmail.com>
2024-04-12 18:49:08 +0300

    add test environment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
+const app = require('./app.js')
 
 app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
