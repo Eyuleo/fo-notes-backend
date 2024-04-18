bd1d983bbec202afb5277b61eb6b35d4af6a05ee
Eyuleo <eyuleo10@gmail.com>
2024-04-18 21:03:31 +0300

    limit note creation to logged in users

--- controllers/login.js
+++ controllers/login.js
@@ -4,14 +4,14 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
-	const { username, password } = req.body
+	const { username, password } = request.body
 
 	const user = await User.findOne({ username })
 	const correctPassword =
 		user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-	if (!(username && password)) {
-		response.status(401).json({
+	if (!(username && correctPassword)) {
+		return response.status(401).json({
 			error: 'invalid username and password',
 		})
 	}
@@ -21,7 +21,9 @@ loginRouter.post('/', async (request, response) => {
 		id: user._id,
 	}
 
-	const token = jwt.sign(userForToken, process.env.SECRET)
+	const token = jwt.sign(userForToken, process.env.SECRET, {
+		expiresIn: 60 * 60,
+	})
 
 	response.status(200).send({ token, username: user.username, name: user.name })
 })
